e1f35d7ef5f64cf2881a0b21042717a1d804be55
Clara Itzel <81541685+ClaraItzel@users.noreply.github.com>
2022-05-13 16:17:21 -0500

    probando

--- classes/propiedad.php
+++ classes/propiedad.php
@@ -2,6 +2,30 @@
 namespace App;
 
 class Propiedad{
-    
+    public $id;
+    public $titulo;
+    public $precio;
+    public $imagen;
+    public $descripcion;
+    public $habitaciones;
+    public $wc;
+    public $estacionamiento;
+    public $creado;
+    public $vendorId;
+
+    public function __construct($args =[])
+    {
+        $this->titulo = $args['titulo'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->titulo = $args['titulo'] ?? ''; 
+        $this->titulo = $args['titulo'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+    }
 }
-?>
+?> 
